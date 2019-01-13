b5e9c48ed06897e507e338bdc61e9ee802953369
João Pedro <jp.bahia.zica@gmail.com>
2019-01-13 16:47:31 -0200

    Moving task model to the correct place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Gerenciador de tarefas'
-  task: Task = new Task(21, 'enviar orçamento para o cliente X')
-}
-
-export class Task{
-  public id: number;
-  public title: string;
-
-  constructor(id: number, title: string){
-    this.id = id
-    this.title = title
-  }
+  title = 'Gerenciador de tarefas';
 }
