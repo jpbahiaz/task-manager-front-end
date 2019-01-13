549195170f8f24150e7cb05a2e5b9553fd3310be
João Pedro <jp.bahia.zica@gmail.com>
2019-01-13 16:38:25 -0200

    Adding TasksComponent

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+// const TASKS: any[] = [];
+const TASKS: Array<any> = [
+  { id: 1, title: 'Fazer tarefa 1' },
+  { id: 2, title: 'Fazer tarefa 2' },
+  { id: 3, title: 'Fazer tarefa 3' },
+  { id: 4, title: 'Fazer tarefa 4' },
+  { id: 5, title: 'Fazer tarefa 5' },
+  { id: 6, title: 'Fazer tarefa 6' },
+  { id: 7, title: 'Fazer tarefa 7' },
+  { id: 8, title: 'Fazer tarefa 8' },
+];
+
+@Component({
+  selector: 'tasks',
+  templateUrl: 'tasks.component.html'
+  // stylesUrl:
+})
+
+export class TasksComponent implements OnInit{
+  public tasks: any[];
+
+  constructor(){  }
+
+  ngOnInit(){
+    this.tasks = TASKS;
+  }
+}
