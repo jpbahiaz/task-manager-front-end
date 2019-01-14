fd904e80e9afea3d7c488e91b148112704193d90
João Pedro <jp.bahia.zica@gmail.com>
2019-01-14 07:23:58 -0200

    binding from logic to view

--- src/app/learning-bindings/learning-bindings.component.ts
+++ src/app/learning-bindings/learning-bindings.component.ts
@@ -7,22 +7,41 @@ import { componentFactoryName } from '@angular/compiler';
 })
 
 export class LearningBindingsComponent{
+    public mouseClickCount: number;
+    public mouseOverCount: number;
+    public userName: string;
+    public userEmail: string;
+
+    constructor(){
+        this.mouseClickCount = 0;
+        this.mouseOverCount = 0;
+        this.userName = 'Nome Inicial';
+        this.userEmail = 'Email Inicial';
+    }
+
+
+
     // mouse events
     public onClick(){
-        console.log("Evento onClick foi acionado")   
+        console.log("Evento onClick foi acionado")
+        this.mouseClickCount++;
     }
 
     public onMouseOver(){
         console.log("Evento onMouseOver foi acionado")
+        this.mouseOverCount++;
     }
 
     // key events
-    public onKeyDown(){
+    public onKeyDown(event: any){
         console.log("Evento de onKeyDown foi acionado")
+        console.log(event)
+        this.userName = event.target.value
     }
 
-    public onKeyUp(){
+    public onKeyUp(event: any){
         console.log("Evento de onKeyUp foi acionado")
+        console.log(event)
+        this.userEmail = event.target.value
     }
-
 }
