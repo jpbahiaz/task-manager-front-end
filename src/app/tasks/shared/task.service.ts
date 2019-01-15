c962b05411e041925b941ab9633804e6388f6e79
João Pedro <jp.bahia.zica@gmail.com>
2019-01-15 08:47:04 -0200

    Adding TaskService and DepedencyInjection

---
+++ src/app/tasks/shared/task.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { Task } from './task.model';
+
+// const TASKS: any[] = [];
+const TASKS: Array<Task> = [
+    { id: 1, title: 'Fazer tarefa 1' },
+    { id: 2, title: 'Fazer tarefa 2' },
+    { id: 3, title: 'Fazer tarefa 3' },
+    { id: 4, title: 'Fazer tarefa 4' },
+    { id: 5, title: 'Fazer tarefa 5' },
+    { id: 6, title: 'Fazer tarefa 6' },
+    { id: 7, title: 'Fazer tarefa 7' },
+    { id: 8, title: 'Fazer tarefa 8' },
+  ];
+
+@Injectable()
+
+export class TaskService{
+    public getTasks(): Array<Task>{
+        return TASKS;
+    }
+}
