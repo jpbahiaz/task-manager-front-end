58f6882512fd71c36de7be68e08e2cf8e3a0367d
João Pedro <jp.bahia.zica@gmail.com>
2019-01-20 18:15:29 -0200

    Adding createTask

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -11,9 +11,11 @@ import { TaskService } from './shared/task.service';
 
 export class TasksComponent implements OnInit{
   public tasks: Task[];
-  public selectedTask: Task;
+  public newTask: Task;
 
-  constructor(private taskService: TaskService){ }
+  constructor(private taskService: TaskService){ 
+    this.newTask = new Task(null, '');
+  }
 
   ngOnInit(){
     this.taskService.getTasks()
@@ -24,7 +26,21 @@ export class TasksComponent implements OnInit{
       )
   }
 
-  public onSelect(task: Task): void{
-    this.selectedTask = task;
+  public createTask(){
+    this.newTask.title = this.newTask.title.trim();
+
+    if(!this.newTask.title){
+      alert("A tarefa deve ter um título")
+    }else{
+      this.taskService.createTask(this.newTask)
+        .subscribe(
+          task => {
+            this.tasks.push(task);
+            this.newTask = new Task(null, '');
+          },
+          error => alert("Ocorreu um erro no servidor tente mais tarde."),
+          () => console.log("Create Tasks Completed")
+        )
+    }
   }
 }
