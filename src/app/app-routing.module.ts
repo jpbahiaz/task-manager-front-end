01c9de24e35eec7ee76e31c0982690984b80db51
João Pedro <jp.bahia.zica@gmail.com>
2019-01-29 20:51:15 -0200

    Adding route for SignInComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
 const ROUTES = RouterModule.forRoot([
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'sign-in', component: SignInComponent },
     { path: 'sign-up', component: SignUpComponent },
     { path: 'tasks', component: TasksComponent },
     { path: 'tasks/:id', component: TaskDetailComponent },
